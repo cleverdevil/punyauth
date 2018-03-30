dbaeb97b203ce496e4f4925f8ad7d7e19df2f64f
Jonathan LaCour <jonathan@cleverdevil.org>
2018-03-30 11:55:28 -0700

    Adding ability to use S3 as a token backend.

--- punyauth/storage.py
+++ punyauth/storage.py
@@ -1,5 +1,10 @@
+from pecan import conf
+
+import json
 import time
 import threading
+import boto3
+import io
 
 
 class TTLCache:
@@ -25,16 +30,37 @@ class TTLCache:
             return self.cache.get(key)
 
 
-cache = TTLCache(ttl=600000)
+class S3Cache:
+
+    def __init__(self, bucket_name):
+        self.bucket_name = bucket_name
+        self.s3 = boto3.client('s3')
+
+    def set(self, key, payload):
+        payload = json.dumps(payload).encode('utf-8')
+        self.s3.put_object(Bucket=self.bucket_name, Key=key, Body=payload)
+
+    def get(self, key):
+        try:
+            response = self.s3.get_object(Bucket=self.bucket_name, Key=key)
+        except:
+            return None
+
+        else:
+            return json.loads(response['Body'].read().decode('utf-8'))
+
+
+if conf.token.get('s3bucket'):
+    cache = S3Cache(conf.token.get('s3bucket'))
+else:
+    cache = TTLCache()
 
 
 def set(payload):
     key = '%s%s%s' % (payload['code'], payload['redirect_uri'], payload['client_id'])
-    print('Setting key: "%s"' % key)
     cache.set(key, payload)
 
 
 def get(payload):
     key = '%s%s%s' % (payload['code'], payload['redirect_uri'], payload['client_id'])
-    print('Searching key: "%s"' % key)
     return cache.get(key)
